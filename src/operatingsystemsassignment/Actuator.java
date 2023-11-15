839cf8390c4d47decd8cf3503e4b6b4bd2c8e191
HythamSoliman <hytham.soliman@gmail.com>
2023-11-15 13:25:22 +0200

    remove extra spaces and lines for better readability

--- src/operatingsystemsassignment/Actuator.java
+++ src/operatingsystemsassignment/Actuator.java
@@ -9,31 +9,26 @@ public class Actuator extends Thread {
 	private Object produceLock;
 	private Object consumeLock;
 	public double currentpos;
-	public double newpos; 
-	public double x; 
-	public double rem; 
+	public double newpos;
+	public double x;
+	public double rem;
 	public double value;
-	public boolean movingRight = true; 
-	public double pos0; 
-	
+	public boolean movingRight = true;
+	public double pos0;
 	
 	public void run(){
-		
-           while (true) {
-   			    consume();
-                try {
-    	
-					Thread.sleep(2000);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
+		while (true) {
+			consume();
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
+		}
 	}
-        
 	
-public Actuator(Queue<Result> qu2, Object produceLock, Object consumeLock, double pos0) {  // actuating constructor 
-		
+	public Actuator(Queue<Result> qu2, Object produceLock, Object consumeLock, double pos0) {  // actuating constructor
 		this.qu2 = qu2;
 		this.produceLock = produceLock;
 		this.consumeLock = consumeLock;
@@ -41,9 +36,7 @@ public Actuator(Queue<Result> qu2, Object produceLock, Object consumeLock, doubl
 	}
 	
     public double getPos0() {
-    	
-    	return this.pos0; 
-    	
+    	return this.pos0;
     }
 
 	public synchronized void consume()   // synchronized consume method 
@@ -51,81 +44,55 @@ public Actuator(Queue<Result> qu2, Object produceLock, Object consumeLock, doubl
         while (true) {
             synchronized (this.consumeLock)
             {
-
                 if (qu2.size() != 0) {
-                  
-
-                Result r = qu2.poll(); 
-                
-                if (r.getID() == 1) {                  // to set the initial position from user input as the first position of the robot
+                	Result r = qu2.poll();
+                	if (r.getID() == 1) {                  // to set the initial position from user input as the first position of the robot
                 		if (r.movedist + pos0 <= 1) {
                     			value = r.movedist + pos0;
                     			newpos = value;
-                    			System.out.println("Robot moving." + " task id {" + r.getID() + "}" + "task complexity {" +  r.getComp() + "} result (distance to move) {" + r.getMoveDist() + "} old position: {" + getPos0() + "} new position: {" + newpos + "}" ); 
-                    			currentpos = newpos; 
-                    			}
-                        	
-                        else  {
-                    			rem = -1 + (r.movedist + pos0); 
-                    			value = 1 - rem; 
-                        		newpos = value;
-                        		System.out.println("Robot moving." + " task id {" + r.getID() + "}" + "task complexity {" +  r.getComp() + "} result (distance to move) {" + r.getMoveDist() + "} old position: {" + getPos0() + "} new position: {" + newpos + "}" ); 
-                        		currentpos = newpos; 
-                        		movingRight = false; 
-                    		
-                }
-           
-                }
-                else {
-            	
-                if (movingRight == true)
-                {
-                	if (r.movedist + currentpos <= 1) {
-            			value = r.movedist + currentpos;
-            			newpos = value;
-            			}
-                	
-                	else  {
-            			rem = -1 + (r.movedist + currentpos); 
-            			value = 1 - rem; 
-                		newpos = value;
-                		movingRight = false; 
-            		}
-                }
-                
-                else
-                    {
-                    	if (currentpos - r.getMoveDist() >= 0) {
-                			value =  currentpos - r.getMoveDist();
-                			newpos = value;
-                			}
-                    	
-                    	else  {
-                			value = Math.abs(currentpos - r.getMoveDist()); 
-                    		newpos = value;
-                    		movingRight = true; 
+                    			System.out.println("Robot moving." + " task id {" + r.getID() + "}" + "task complexity {" +  r.getComp() + "} result (distance to move) {" + r.getMoveDist() + "} old position: {" + getPos0() + "} new position: {" + newpos + "}" );
+                    			currentpos = newpos;
+						} else  {
+							rem = -1 + (r.movedist + pos0);
+							value = 1 - rem;
+							newpos = value;
+							System.out.println("Robot moving." + " task id {" + r.getID() + "}" + "task complexity {" +  r.getComp() + "} result (distance to move) {" + r.getMoveDist() + "} old position: {" + getPos0() + "} new position: {" + newpos + "}" );
+							currentpos = newpos;
+							movingRight = false;
                 		}
-                }
-          		System.out.println("Robot moving." + " task id {" + r.getID() + "}" + "task complexity {" +  r.getComp() + "} result (distance to move) {" + r.getMoveDist() + "} old position: {" + currentpos + "} new position: {" + newpos + "}" ); 
-          		currentpos = newpos;  // updating the current position as the new one for the next one 
-                } 		
-                }
-
-                
+                	} else {
+						if (movingRight == true)
+						{
+							if (r.movedist + currentpos <= 1) {
+								value = r.movedist + currentpos;
+								newpos = value;
+								}
+							
+							else  {
+								rem = -1 + (r.movedist + currentpos);
+								value = 1 - rem;
+								newpos = value;
+								movingRight = false;
+							}
+						} else {
+							if (currentpos - r.getMoveDist() >= 0) {
+								value =  currentpos - r.getMoveDist();
+								newpos = value;
+							}
+							else  {
+								value = Math.abs(currentpos - r.getMoveDist());
+								newpos = value;
+								movingRight = true;
+							}
+						}
+						System.out.println("Robot moving." + " task id {" + r.getID() + "}" + "task complexity {" +  r.getComp() + "} result (distance to move) {" + r.getMoveDist() + "} old position: {" + currentpos + "} new position: {" + newpos + "}" );
+						currentpos = newpos; // updating the current position as the new one for the next one 
+					}
+				}
 				synchronized (this.produceLock) {
 		            produceLock.notifyAll();
-		           }
-        
-		
+		        }
         	}
         }
     }
 }
-	
-
-		
-		
-	
-
-	
-
--- src/operatingsystemsassignment/Analyzer.java
+++ src/operatingsystemsassignment/Analyzer.java
@@ -1,112 +1,65 @@
 package operatingsystemsassignment;
 import java.util.Queue;
+public class Analyzer extends Thread {
+	public Queue<Task> q1;
+	public Queue<Result> q2;
+	public int capacity = 5;
+	public int currentpos = 0;
+	public int analysisConstant;
 
+	public Analyzer(Queue<Task> q1, Queue<Result> q2, int analysisConstant) {   // analyzer constructor
+		this.q1 = q1;
+		this.q2 = q2;
+		this.analysisConstant = analysisConstant;
+	}
 
-            public class Analyzer extends Thread {    
-            	public Queue<Task> q1;
-                public Queue<Result> q2;
-                public int capacity = 5;
-                public int currentpos = 0; 
-                public int analysisConstant; 
+	@Override
+	public void run() {     // run method for analyzer
+		while (true) {
+			consume();
+		}
+	}
 
-                
-                public Analyzer(Queue<Task> q1, Queue<Result> q2, int analysisConstant) {   // analyzer constructor 
-                	
-                	this.q1 = q1; 
-                	this.q2 = q2; 
-                	this.analysisConstant = analysisConstant; 
-                	
-                }
-                
-                @Override
-            	public void run() {     // run method for analyzer
-                    
-                	while (true) {
-                	consume();         
-                	
-            	            } 
-                	
-            	}
-            	
-            	
-            	
+	public synchronized void consume() 
+	{
+		if (q1.size() != 0) {            // condition checking if q1 is not empty 
+			Task t = q1.poll();          // takes out the element at the front of the queue and returns it, assigning it as "t"
+			Result r = new Result(t.getRandomID(), t.getRandomComplexity(), Math.pow((1 - t.getRandomComplexity()), analysisConstant)); // creating the result, which takes the Id and c from the task 
+			if (q2.size() != capacity) {       // if q2 is not full 
+				q2.add(r);   // add result to q2 
+				System.out.println("analyzing task " + r.getID());
+			}
+			try {
+				Thread.sleep((int)(t.getRandomComplexity())*100);
+			}
+			catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
 
-				public synchronized void consume() 
-                {
-					
-                    if (q1.size() != 0) {            // condition checking if q1 is not empty 
-                    	Task t = q1.poll();           // takes out the element at the front of the queue and returns it, assigning it as "t"
-                    	Result r = new Result(t.getRandomID(), t.getRandomComplexity(), Math.pow((1 - t.getRandomComplexity()), analysisConstant));  // creating the result, which takes the Id and c from the task 
-                    	if (q2.size() != capacity) {       // if q2 is not full 
-                    		
-                    		
-                    		
-                    		q2.add(r);    // add result to q2 
-                    		System.out.println("analyzing task " + r.getID());
-                    		
-                    		
-                    	}
-                     	try {
-                        	  
-                    		Thread.sleep((int)(t.getRandomComplexity())*100);
-                    		 
-                    	
-                    	}
-                	                catch (InterruptedException e) {
-                	                    e.printStackTrace();
-                	                    
-                	                }
-                             }
-               
-                        }
-				
-                     
-                       
-                    }
-
-
-                        class Result { 
-                        public double id;
-                        public double comp;
-                	    public double movedist;
-                	    public double analysisConstant; 
-                		
-                	    public Result(double id, double comp, double movedist) {
-                			this.id = id;
-                			this.comp = comp;
-                			this.movedist = movedist;
-                			 
-                			
-                		}
-                	 
-                	   
-                		
-                	    public double getID() {
-                	    	
-                	    	return this.id;
-                	    }
-                	    
-                        public double getComp() {
-                	    	
-                	    	return this.comp;
-                	    }
-                	
-                       
-                        
-                        public double getMoveDist() {
-                	    	
-                	    	return this.movedist;
-                	    }
-                        
-                
-                        
-                        public double getAnalysisConstant() {
-                        	
-                        	return this.analysisConstant; 
-                        	
-                        }
-                	
-                	    
-                	}
-                	   
-                	   
+class Result { 
+	public double id;
+	public double comp;
+	public double movedist;
+	public double analysisConstant;
+	
+	public Result(double id, double comp, double movedist) {
+		this.id = id;
+		this.comp = comp;
+		this.movedist = movedist;
+	}
+	public double getID() {
+		return this.id;
+	}
+	public double getComp() {
+		return this.comp;
+	}
+	public double getMoveDist() {
+		return this.movedist;
+	}
+	public double getAnalysisConstant() {
+		return this.analysisConstant;
+	}
+}
